c3b714d49ba9e85241666334b26c14ea994dad69
Max <gccccvbnnnn@mail.ru>
2025-03-28 17:36:29 +0700

    fix

--- lib/debounce-func.ts
+++ lib/debounce-func.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function debounce<T extends (...args: any[]) => void>(func: T, wait: number) {
-    let timeout: NodeJS.Timeout;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let timeout: any;
     return function (...args: Parameters<T>): void {
         clearTimeout(timeout);
         timeout = setTimeout(() => func(...args), wait);
