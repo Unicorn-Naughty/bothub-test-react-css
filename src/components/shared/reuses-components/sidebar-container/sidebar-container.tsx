c4594d28cb6cec7387a6c4f91ccad048e37caf5d
Max <gccccvbnnnn@mail.ru>
2025-03-28 17:27:59 +0700

    fix

--- src/components/shared/reuses-components/sidebar-container/sidebar-container.tsx
+++ src/components/shared/reuses-components/sidebar-container/sidebar-container.tsx
@@ -2,6 +2,7 @@ import { sideBarStateStore } from "@/src/store/sidebar-state-store";
 import { cn } from "@/lib/utils";
 import { useClickAway } from "react-use";
 import React from "react";
+import { motion } from "framer-motion";
 import styles from "./index.module.css";
 
 interface Props {
